3af1de2b3dd68a2768e84ce525319f1d3c7bf881
Edi Amin <to.ediamin@gmail.com>
2023-10-06 18:31:06 +0600

    Remove class completion provider for now

--- src/class-aggregator.ts
+++
@@ -1,7 +0,0 @@
-function classAggregator(): Thenable< string[] > {
-	return new Promise( ( resolve ) => {
-		resolve( [ 'has-black-background-color', 'has-white-background-color' ] );
-	} );
-}
-
-export default classAggregator;
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 import { ExtensionContext, StatusBarAlignment, commands, languages, window, workspace } from 'vscode';
 
-import { ClassCompletionItemProvider } from './class-completion-item-provider';
 import { CssVariableCompletionItemProvider } from './css-variable-completion-item-provider';
 import { CssVariableHoverProvider } from './css-variable-hover-provider';
 import refreshProviders from './refresh-providers';
@@ -32,7 +31,6 @@ export async function activate( context: ExtensionContext ) {
 	// Providers.
 	const cssVariableCompletionProvider = new CssVariableCompletionItemProvider();
 	const cssVariableHoverProvider = new CssVariableHoverProvider();
-	const classCompletionProvider = new ClassCompletionItemProvider();
 
 	const doRefreshProviders = () => {
 		let timeout = null;
@@ -66,7 +64,6 @@ export async function activate( context: ExtensionContext ) {
 		setThemeJsonPathCommand,
 		languages.registerCompletionItemProvider( documentSelectors, cssVariableCompletionProvider ),
 		languages.registerHoverProvider( documentSelectors, cssVariableHoverProvider ),
-		languages.registerCompletionItemProvider( documentSelectors, classCompletionProvider ),
 		// Referesh providers everytime we save the theme.json.
 		workspace.onDidSaveTextDocument( ( document ) => {
 			const projectRoot = workspace.workspaceFolders?.[ 0 ].uri.path ?? '';
