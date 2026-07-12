63e391c237ab85cdba7dea0c1a26a94dc054692a
Edi Amin <to.ediamin@gmail.com>
2026-07-12 21:19:17 +0600

    Show RGB alongside hex color docs
    Add hex-to-RGB conversion for color completion docs in `get-css-variable-doc`. Color items now display both the original hex value and its RGB equivalent when the value is a valid 3- or 6-digit hex code.
    
    Closes #2

--- src/get-css-variable-doc.ts
+++ src/get-css-variable-doc.ts
@@ -2,12 +2,33 @@ import { CompletionItemKind, MarkdownString } from 'vscode';
 
 import type { CssVariableItem } from './types';
 
+function getRgbValue( value: string ): string | null {
+	const match = value.match( /^#([\da-f]{3}|[\da-f]{6})$/i );
+
+	if ( ! match ) {
+		return null;
+	}
+
+	const hex =
+		match[ 1 ].length === 3
+			? match[ 1 ]
+					.split( '' )
+					.map( ( character ) => character.repeat( 2 ) )
+					.join( '' )
+			: match[ 1 ];
+	const channels = hex.match( /.{2}/g );
+
+	return channels ? `rgb(${ channels.map( ( channel ) => parseInt( channel, 16 ) ).join( ', ' ) })` : null;
+}
+
 function getCssVariableDoc( details: CssVariableItem ): MarkdownString {
 	let documentation = '';
 
 	switch ( details.kind ) {
 		case CompletionItemKind.Color:
-			documentation = `<span style="background-color:${ details.value };">&nbsp;&nbsp;&nbsp;&nbsp;</span>&nbsp;&nbsp;${ details.value }`;
+			const rgbValue = getRgbValue( details.value );
+			const colorValue = rgbValue ? `${ details.value } (${ rgbValue })` : details.value;
+			documentation = `<span style="background-color:${ details.value };">&nbsp;&nbsp;&nbsp;&nbsp;</span>&nbsp;&nbsp;${ colorValue }`;
 			break;
 		default:
 			documentation = details.value;
